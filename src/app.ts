cb790799d9f47a6a3515d8a3880b404d83db5419
Matias Torres Contreras <tcontreras993@gmail.com>
2025-04-11 17:18:48 -0600

    Se creo la coneccion a la DB de mongoDB

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import { envs } from "./config/envs";
+import { MongoDatabase } from "./data";
 import { AppRoutes } from "./presentation/routes";
 import { Server } from "./presentation/sertver";
 
@@ -6,7 +7,12 @@ import { Server } from "./presentation/sertver";
   main();
 })();
 
-function main() {
+async function main() {
+  await MongoDatabase.connect({
+    mongoUrl: envs.MONGO_URL,
+    dbName: envs.MONGO_DB_NAME,
+  });
+
   const server = new Server({
     port: envs.PORT,
     routes: AppRoutes.routes,
