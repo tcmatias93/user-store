dde5d27f9ae164b8fd4ddf97ceb7c1cdbf632a85
Matias Torres Contreras <tcontreras993@gmail.com>
2025-04-11 15:59:32 -0600

    Primer commit y creacion del proyecto

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+import { envs } from "./config/envs";
+import { AppRoutes } from "./presentation/routes";
+import { Server } from "./presentation/sertver";
+
+(async () => {
+  main();
+})();
+
+function main() {
+  const server = new Server({
+    port: envs.PORT,
+    routes: AppRoutes.routes,
+  });
+
+  server.start();
+}
